f1b6eda79a49c576abedd2628281b210b1ad26b6
Flere21 <aaronhans2134@gmail.com>
2023-04-24 13:34:07 +0800

    checks each number from integer.txt and sorts them into the list

--- int2squareandcube.py
+++ int2squareandcube.py
@@ -5,5 +5,12 @@ with open("integers.txt") as my_file:
 even_numbers = []
 odd_numbers = []
 #check each number and sort them in the lists
+for i in numbers:
+#if even add in even number list
+    if int(i) % 2 == 0:
+        even_numbers.append(i)
+#if odd add in odd number list
+    else:
+        odd_numbers.append(i)
 #create double.txt for even numbers getting square
 #create triple.txt for odd numbers getting cube
